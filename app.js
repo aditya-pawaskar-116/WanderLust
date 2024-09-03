fbdc70cc8ebd006ed4a525e09021c1bfc52d6470
aditya-pawaskar-116 <157811189+aditya-pawaskar-116@users.noreply.github.com>
2024-09-03 18:09:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ app.all("*", (req, res, next) => {
  app.use((err, req, res, next) => {
      console.log(err);
     let {statusCode=500, message="Something went wrong!" } = err;
-    res.status(statusCode).render("listings/error.ejs", {message});
+    res.status(statusCode).render("error.ejs", {message});
 });
 
 dbConnection();
